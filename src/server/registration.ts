1ee7920b3c5832141163425421479fde4d768c61
Anthony Camarillo <anthony.camarillo.96@gmail.com>
2023-01-11 01:21:00 -0800

    (feat): add basic registration page

--- src/server/registration.ts
+++ src/server/registration.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response, NextFunction, Router } from 'express';
+import path from 'path';
 
 const router: Router = express.Router();
 
+const registrationPage: string = path.join(__dirname,'client/registration.html')
+
 router.get('/', (req: Request, res: Response) => {
-    res.set('Content-Type','text/html');
     res.status(200);
-    res.send("<html><body><h1>This is the registration page</h1></body></html>");
+    res.sendFile(registrationPage);
 })
 
 
