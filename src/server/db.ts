2181fcba21c063779d5a0617ce401ba85319c120
Anthony Camarillo <anthony.camarillo.96@gmail.com>
2023-01-16 15:12:39 -0800

    Add configuration file for SQlite database

---
+++ src/server/db.ts
@@ -0,0 +1,23 @@
+import { Sequelize, Model, DataTypes, InferAttributes, InferCreationAttributes } from 'sequelize';
+
+const sequelize: Sequelize = new Sequelize('database','username','password', {
+    dialect: 'sqlite',
+    storage: './dist/db.sqlite'
+});
+
+interface UserModel extends Model<InferAttributes<UserModel>, InferCreationAttributes<UserModel>> {
+    username: string;
+    password: string;
+}
+
+const UserModel = sequelize.define<UserModel>('User', {
+    username: {
+        type: DataTypes.STRING,
+        unique: true,
+    },
+    password: { 
+        type: DataTypes.STRING,
+    }
+});
+
+export default UserModel;
