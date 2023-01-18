aaa0096a53672b59fc7cdd07a6853ee1beeb80aa
Anthony Camarillo <anthony.camarillo.96@gmail.com>
2023-01-18 11:26:45 -0800

    (feat): Add POST method for registration
    Add functionality for using sequelize to create new users in sqlite
    database

--- src/server/registration.ts
+++ src/server/registration.ts
@@ -1,14 +1,39 @@
 import express, { Request, Response, NextFunction, Router } from 'express';
-import path from 'path';
+import UserModel from './db.js';
+import { UniqueConstraintError } from '@sequelize/core';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-const router: Router = express.Router();
+// __dirname equivalent for ES Modules
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const registration: Router = express.Router();
 
 const registrationPage: string = path.join(__dirname,'client/registration.html')
 
-router.get('/', (req: Request, res: Response) => {
+registration.get('/', (req: Request, res: Response) => {
     res.status(200);
     res.sendFile(registrationPage);
 })
 
+registration.post('/', async (req: Request, res: Response) => {
 
-export default router;
+    const username = req.body.username;
+    const password = req.body.password;
+    try {
+        await UserModel.sync({});
+        await UserModel.create({
+            username: username,
+            password: password
+        });
+        res.status(201);
+        res.redirect('login');
+    }
+    catch (err: any) {
+        if (err.name === 'SequelizeUniqueConstraintError') {
+            res.status(409).send('That username is already taken!');
+        }
+    }
+})
+
+export default registration;
