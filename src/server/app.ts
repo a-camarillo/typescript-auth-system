064dc33545bade58b0924715a0a9a60d74b14ab7
Anthony Camarillo <anthony.camarillo.96@gmail.com>
2023-01-18 11:30:01 -0800

    (chore): Add encoding middleware to server

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response, NextFunction } from 'express';
-import registration from './registration';
-import login from './login';
+import registration from './registration.js';
+import login from './login.js'; 
 
 const app = express();
 const port: number = 3000;
 
+app.use(express.urlencoded({ extended: true}))
 app.use('/registration',registration);
 app.use('/login', login);
 
