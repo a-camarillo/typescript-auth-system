445043d019c9555b7a546377c4d984cc6b7b15d2
Anthony Camarillo <anthony.camarillo.96@gmail.com>
2023-01-23 08:15:29 -0800

    Add login functionality

--- src/server/login.ts
+++ src/server/login.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, Router } from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import UserModel from './db.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -11,6 +12,26 @@ const loginPage: string = path.join(__dirname,'/client/login.html')
 login.get('/', (req: Request, res: Response) => {
     res.status(200);
     res.sendFile(loginPage)
+})
+
+login.post('/', async (req: Request, res: Response) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try {
+        await UserModel.sync({});
+        await UserModel.findAll({
+            attributes: ['username','password'],
+            where: {
+                username: username,
+                password: password
+            }
+        })
+        res.status(301).redirect('/');
+    }
+    catch (err: any) {
+
+    }
 })
 
 export default login;
